80a9df96ce5e88343b5c1d6c4bdaa0ef2059ce38
Thomas Petiteau <tpetiteau@cegid.com>
2024-01-12 21:50:55 +0100

    added keymap in neovim

--- .config/nvim/lua/xconfig/packer.lua
+++ .config/nvim/lua/xconfig/packer.lua
@@ -427,6 +427,8 @@ return packer.startup(
 
                 vim.keymap.set("n", "<leader>ff", builtin.find_files)
                 vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+                vim.keymap.set("n", "<leader>fr", builtin.lsp_references)
+                vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols)
             end
         }
 
